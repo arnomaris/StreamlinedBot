4225238e6861c85992a50f43e43fbce7b805808b
Arno Maris <arnomaris@hotmail.com>
2020-09-17 14:46:19 +0200

    Added new suggestion systen

--- Bot.js
+++ Bot.js
@@ -1,28 +1,28 @@
 var discord = require('discord.js');
-var mysql = require('mysql');
 var client = new discord.Client();
 var token = 'MzEyOTIxODY0MDA1NDg0NTQ1.DmNMMQ.uhN0FtBvisVROznbrGleGBAXEiU'
+var pastebintoken = "b85a1fde3f69f6c3f7353c234e13f666"
 var prefix = "!"
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
-  });
-
 var photoContestChannel = '742420136488599653'
+var streamlinedGuild
+var suggestionRules
 
 client.login(token)
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity(" fetch with doggo cows")
         .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .catch(console.error);
+
+    streamlinedGuild = client.guilds.cache.get("424256218916454411")
+        
+    suggestionRules = client.channels.cache.get("755911576510398504");
+    if (!suggestionRules) return console.log("Invalid channel.");
+
+    suggestionRules.messages.fetch("756132113421566125")
+        .then(message => console.log("Got suggestion rules message!"))
         .catch(console.error);
 });
 
@@ -31,7 +31,7 @@ function pluck(array){
 }
 
 function hasRole(members, role){
-    if(pluck(members.roles.cache).includes(role)){
+    if(members && pluck(members.roles.cache).includes(role)){
         return true;
     } else {
         return false;
@@ -39,32 +39,53 @@ function hasRole(members, role){
 }
 
 function isAdmin(message){
-	if(
-		hasRole(message.member,"Lead Developer") ||
-		hasRole(message.member,"Streamlined Owner")
-		){
-
+	if(hasRole(message.member,"Lead Developer") || hasRole(message.member,"Streamlined Owner")){
 		return true;
-	} else {
+	}else{
 		return false;
 	}
 }
 
 function isCommand(command, message){
 	var command = command.toLowerCase();
-	var content = message.content.toLowerCase();
+	const content = message.content.toLowerCase();
 	return content.startsWith(prefix + command);
 }
+
 
 client.on('message', (message) => {
     if (message.channel.id == photoContestChannel){
         message.react('👍')
         .catch(console.error);
     };
-    //if(isAdmin(message)){
-        //if(isCommand("cheese", message)){
-            //var generalChannel = client.channels.cache.find(channel => channel.name === "general")
-            //generalChannel.send("I like cheese")
-        //;
-    //};
+    if(isAdmin(message)){
+        if(isCommand("cheese", message)){
+            const generalChannel = client.channels.cache.find(channel => channel.name === "general")
+            generalChannel.send("I like cheese")
+        };
+    };
 });
+
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    if (messageReaction.message.id === "756132113421566125"){
+        const member = streamlinedGuild.members.cache.get(user.id)
+        if (!member.roles.cache.get("755934517906112555")){
+            member.roles.add(streamlinedGuild.roles.cache.get("755899895239540826"))
+        } else {
+            try {
+                messageReaction.users.remove(user.id);
+            } catch (error) {
+                console.error('Failed to remove reactions.');
+            }
+        }
+    }
+});
+
+client.on('messageReactionRemove', (messageReaction, user) => {
+    if (messageReaction.message.channel.id === "755911576510398504"){
+        streamlinedGuild.members.cache.get(user.id).roles.remove(streamlinedGuild.roles.cache.get("755899895239540826"))
+    }
+});
+
+client.guilds
