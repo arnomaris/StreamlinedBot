a77adbc6f61829c18d2516f13b03603065cb65b8
arnomaris <arnomaris@hotmail.com>
2021-04-09 15:01:11 +0200

    Readd getnitrostatus command

--- discord/commands/commands.js
+++ discord/commands/commands.js
@@ -2,6 +2,7 @@ module.exports = {
     cheese: require('./cheese.js'),
     openvoting: require('./openvoting.js'),
     isbooster: require('./getnitrostatus.js'),
+    getnitrostatus: require('./getnitrostatus.js'),
     game: require('./infoMessage.js'),
     roadmap: require('./infoMessage.js'),
     wiki: require('./infoMessage.js'),
