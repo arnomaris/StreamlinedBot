151a12bd0dd76e6ecd6f2e4b1037dbaf87635334
arnomaris <arnomaris@hotmail.com>
2021-06-19 10:34:41 +0200

    Fixed cheese

--- discord/commands/cheese.js
+++ discord/commands/cheese.js
@@ -1,8 +1,9 @@
 const commandUtil = require('./../utility/commandUtil.js')
+const clientHandler = require('./../client.js')
 
 module.exports = function(message) {
     if(commandUtil.isAdmin(message)){
-        const generalChannel = client.channels.cache.find(channel => channel.name === 'general')
+        const generalChannel = clientHandler.client.channels.cache.find(channel => channel.name === 'general')
         generalChannel.send('I like cheese')
     };
 }
