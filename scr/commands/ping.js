07de62378890612b172ccb3c805b0f60b24a2d4b
arnomaris <arnomaris@hotmail.com>
2022-10-01 00:53:28 +0200

    Fixed ping command

--- scr/commands/ping.js
+++ scr/commands/ping.js
@@ -6,6 +6,6 @@ module.exports = {
         .setDescription('Get the link to the roadmap'),
     async execute(interaction) {
         let response = await interaction.reply({ content: "Pinging..", ephemeral: false })
-		await interaction.editReply(`🏓**Pong!**\n**Latency**\n${SnowflakeUtil.deconstruct(response.id).timestamp - SnowflakeUtil.deconstruct(interaction.id).timestamp}ms\n**API**\n${Math.round(Client.ws.ping)}ms`);
+		await interaction.editReply(`🏓**Pong!**\n**Latency**\n${SnowflakeUtil.deconstruct(response.id).timestamp - SnowflakeUtil.deconstruct(interaction.id).timestamp}ms\n**API**\n${Math.round(interaction.client.ws.ping)}ms`);
     }
 };
