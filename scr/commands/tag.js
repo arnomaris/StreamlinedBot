db130522bf60ce5b4da72c736777184a5e29d767
Arno Maris <arno.maris@student.kuleuven.be>
2023-03-07 18:08:51 +0100

    Tag command

---
+++ scr/commands/tag.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, SnowflakeUtil } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('tag')
+        .setDescription('Apply tag to forum channel')
+        .addStringOption(option =>
+			option.setName('tag')
+				.setDescription('The applied tag')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Not tested', value: 'Not tested' },
+					{ name: 'Testing', value: 'Testing' },
+					{ name: 'Tested', value: 'Tested' },
+					{ name: 'Broken', value: 'Broken' },
+				)),
+    async execute(interaction) {
+        if (interaction.channel.isThread() && interaction.channel.name == "testing") {
+            const tag = interaction.options.getString('tag');
+            interaction.channel.setAppliedTags([tag])
+            interaction.reply({ content: "Applied tag!", ephemeral: true })
+        } else {
+            interaction.reply({ content: "This command does not work in this channel!", ephemeral: true })
+        }
+    }
+};
