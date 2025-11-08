73647104120c0dbea6ac253fdd181d028dbbbea4
Arno Maris <arnomaris@hotmail.com>
2025-11-08 16:17:41 +0100

    Simplify code

--- src/commands/isbooster.js
+++ src/commands/isbooster.js
@@ -6,20 +6,11 @@ module.exports = {
         .setDescription('Get nitro status of member')
         .addUserOption(option => option.setName('member').setDescription('Member')),
     async execute(interaction) {
-        const member = interaction.options.getMember('member')
-        if (member) {
-            if (member.premiumSince) {
-                interaction.reply(`${member.displayName} has been boosting since ${member.premiumSince.toLocaleDateString("nl-NL")}`)
-            } else {
-                interaction.reply(`${member.displayName} is not a booster <:doggosad:610744652781322251>`)
-            }
+        const member = interaction.options.getMember('member') ?? interaction.member
+        if (member.premiumSince) {
+            interaction.reply(`${member.displayName} has been boosting since ${member.premiumSince.toLocaleDateString("nl-NL")}`)
         } else {
-            let commandMember = interaction.member
-            if (commandMember.premiumSince) {
-                interaction.reply(`${commandMember.displayName} has been boosting since ${commandMember.premiumSince.toLocaleDateString("nl-NL")}`)
-            } else {
-                interaction.reply(`${commandMember.displayName} is not a booster <:doggosad:610744652781322251>`)
-            }
+            interaction.reply(`${member.displayName} is not a booster <:doggosad:610744652781322251>`)
         }
     }
 };
